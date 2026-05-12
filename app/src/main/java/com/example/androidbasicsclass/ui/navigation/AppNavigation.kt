29c73145d41e3d4ef97e07c8c32ae97c0167efd7
Cristhian Loya <15186@ulsachihuahua.edu.mx>
2026-05-11 18:18:18 -0600

    Navigation

--- app/src/main/java/com/example/androidbasicsclass/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/androidbasicsclass/ui/navigation/AppNavigation.kt
@@ -3,10 +3,12 @@ package com.example.androidbasicsclass.ui.navigation
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccessAlarms
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.PhoneAndroid
 import androidx.compose.material.icons.filled.School
 import androidx.compose.material.icons.filled.Smartphone
+import androidx.compose.material.icons.outlined.Accessibility
 import androidx.compose.material.icons.outlined.PhoneAndroid
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
@@ -36,8 +38,8 @@ import com.example.androidbasicsclass.ui.thirdpartialpdm1.homeThirdPartialPDM1.v
  */
 sealed class AppRoute(val route: String, val label: String, val icon: ImageVector) {
     object ThirdPartialIDS2 : AppRoute("third_partial_ids2", "IDS2 P3", Icons.Filled.School)
-    object FirstPartialPDM1 : AppRoute("first_partial_pdm1", "PDM1 P1", Icons.Filled.PhoneAndroid)
-    object SecondPartialPDM1 : AppRoute("second_partial_pdm1", "PDM1 P2", Icons.Outlined.PhoneAndroid)
+    object FirstPartialPDM1 : AppRoute("first_partial_pdm1", "PDM1 P1", Icons.Filled.AccessAlarms)
+    object SecondPartialPDM1 : AppRoute("second_partial_pdm1", "PDM1 P2", Icons.Outlined.Accessibility)
     object ThirdPartialPDM1 : AppRoute("third_partial_pdm1", "PDM1 P3", Icons.Filled.Smartphone)
     object PersonalInformation : AppRoute("personal_information", "About Me", Icons.Filled.Person)
 }
